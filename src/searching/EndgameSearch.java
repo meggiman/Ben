34a75dded18bfe4f05e4fbf4a9d54a6dfd4dd560
Manuel Eggimann <meggimann@student.ethz.ch>
2014-05-16 23:46:41 +0200

    Copied EndgameSearch class from Branch Manuel to this Branch

--- src/searching/EndgameSearch.java
+++ src/searching/EndgameSearch.java
@@ -3,178 +3,255 @@ package searching;
 import Gameboard.Bitboard;
 
 public class EndgameSearch{
-    long nextMove(Bitboard gb, boolean player){
-        return 0;
+    public final static int  NORESULT        = -2147483648;
+    public static int        outCome;
+    public static long       nodeCount;
+    public static long       deadline;
+    private static boolean   timeoutNoResult = false;
+    public static MoveList[] moveListPool    = MoveList.generateListPool(30, 25);
+
+    public final static long solveTree(long red, long green, int alpha, int beta){
+        nodeCount = 0;
+        timeoutNoResult = false;
+        int remainingStones = 64 - Long.bitCount(red)
+                - Long.bitCount(green);
+        long possibleMoves = Bitboard.possibleMovesRed(red, green);
+        if(possibleMoves == 0){
+            long possibleMovesEnemy = Bitboard.possibleMovesRed(green, red);
+            if(possibleMovesEnemy != 0){
+                outCome = min(red, green, alpha, beta, remainingStones, Bitboard.possibleMovesRed(green, red));
+            }
+            return 0;
+        }
+        long bestmove = Long.highestOneBit(possibleMoves);
+        int bestvalue = alpha;
+        int value = alpha;
+        MoveList moveList = MoveList.recycle(moveListPool[remainingStones], possibleMoves, red, green);
+        for (int i = 0; i < moveList.size; i++){
+            int index = moveList.orderedIndices[i];
+            value = min(moveList.red[index], moveList.green[index], value, beta, remainingStones - 1, moveList.possiblemoves[index]);
+            if(value > bestvalue){
+                if(value >= beta){
+                    bestvalue = value;
+                    outCome = value;
+                    return moveList.move[index];
+                }
+                bestvalue = value;
+                bestmove = moveList.move[index];
+            }
+            possibleMoves ^= moveList.move[index];
+        }
+        outCome = bestvalue;
+        if(timeoutNoResult){
+            outCome = NORESULT;
+        }
+        return bestmove;
     }
 
-    public static class OutcomeSearch{
-        public static int  outcome;
-        public static long nodeCount;
-        public static int  remainingStones;
-
-        public final static long nextMove(Bitboard gb){
-            nodeCount = 0;
-            remainingStones = gb.countStones(true) + gb.countStones(false);
-            long possibleMoves = gb.getPossibleMoves(true);
-            if(possibleMoves == 0){
-                long possibleMovesEnemy = gb.getPossibleMoves(false);
-                if(possibleMovesEnemy != 0){
-                    outcome = (int) min(gb, (byte) -1, (byte) 1);
-                }
-                return 0;
-            }
-            long bestMove = Long.highestOneBit(possibleMoves);
-            byte alpha = (byte) -1;
-            while(possibleMoves != 0){
-                long coords = Long.highestOneBit(possibleMoves);
-                long changedFields = gb.makeMove(true, coords);
-                byte value = min(gb, alpha, (byte) 1);
-                gb.undoMove(changedFields, coords, true);
-                if(value == 1){
-                    outcome = 1;
-                    return coords;
-                }
-                if(value == 0){
-                    outcome = 0;
-                    bestMove = coords;
-                    alpha = 0;
-                }
-                possibleMoves ^= coords;
-            }
-            outcome = -1;
-            return bestMove;
+    private final static int min(long red, long green, int alpha, int beta, int remainingstones, long possibleMoves){
+        nodeCount++;
+        if(timeoutNoResult){
+            return NORESULT;
         }
-
-        final static byte min(Bitboard gb, byte alpha, byte beta){
-            nodeCount++;
-            long possibleMoves = gb.getPossibleMoves(false);
-            if(possibleMoves == 0){
-                if(gb.getPossibleMoves(true) == 0){
-                    int myStones = gb.countStones(false);
-                    int opponentStones = gb.countStones(true);
-                    if(myStones > opponentStones){
-                        return (byte) -1;
-                    }
-                    else if(opponentStones > myStones){
-                        return (byte) 1;
-                    }
-                    else{
-                        return 0;
-                    }
-                }
-                return max(gb, alpha, beta);
+        if(remainingstones <= 4){
+            if(System.nanoTime() >= deadline){
+                timeoutNoResult = true;
             }
-            if(alpha == -1 && beta == 1){
-                do{
-                    long coords = Long.highestOneBit(possibleMoves);
-                    long changedFields = gb.makeMove(false, coords);
-                    byte value = max(gb, alpha, beta);
-                    gb.undoMove(changedFields, coords, false);
-                    if(value == -1){
-                        return -1;
-                    }
-                    possibleMoves ^= coords;
-                    if(value == 0){
-                        beta = 0;
-                        break;
-                    }
-                }while(possibleMoves != 0);
-            }
-            if(beta == 0){
-                while(possibleMoves != 0){
-                    long coords = Long.highestOneBit(possibleMoves);
-                    long changedFields = gb.makeMove(false, coords);
-                    if(max(gb, (byte) -1, (byte) 0) == -1){
-                        gb.undoMove(changedFields, coords, false);
-                        return -1;
-                    }
-                    gb.undoMove(changedFields, coords, false);
-                    possibleMoves ^= coords;
-                }
-                return 0;
-            }
-            if(alpha == 0){
-                do{
-                    long coords = Long.highestOneBit(possibleMoves);
-                    long changedFields = gb.makeMove(false, coords);
-                    if(max(gb, (byte) 0, (byte) 1) <= 0){
-                        gb.undoMove(changedFields, coords, false);
-                        return 0;
-                    }
-                    gb.undoMove(changedFields, coords, false);
-                    possibleMoves ^= coords;
-                }while(possibleMoves != 0);
-                return 1;
-            }
-            return 1;
+            return finalScorefewremainingMin(red, green, alpha, beta, false);
         }
-
-        final static byte max(Bitboard gb, byte alpha, byte beta){
-            nodeCount++;
-            long possibleMoves = gb.getPossibleMoves(true);
-            if(possibleMoves == 0){
-                if(gb.getPossibleMoves(false) == 0){
-                    int myStones = gb.countStones(true);
-                    int opponentStones = gb.countStones(false);
-                    if(myStones > opponentStones){
-                        return (byte) 1;
-                    }
-                    else if(opponentStones > myStones){
-                        return (byte) -1;
-                    }
-                    else{
-                        return 0;
-                    }
-                }
-                return min(gb, alpha, beta);
+        if(possibleMoves == 0){
+            if(Bitboard.possibleMovesRed(red, green) == 0){
+                return Long.bitCount(red) - Long.bitCount(green);
             }
-            if(alpha == -1 && beta == 1){
-                do{
-                    long coords = Long.highestOneBit(possibleMoves);
-                    long changedFields = gb.makeMove(true, coords);
-                    byte value = min(gb, alpha, beta);
-                    gb.undoMove(changedFields, coords, true);
-                    if(value == 1){
-                        return 1;
-                    }
-                    possibleMoves ^= coords;
-                    if(value == 0){
-                        alpha = 0;
-                        break;
-                    }
-                }while(possibleMoves != 0);
-            }
-            if(alpha == 0){
-                while(possibleMoves != 0){
-                    long coords = Long.highestOneBit(possibleMoves);
-                    long changedFields = gb.makeMove(true, coords);
-                    if(min(gb, (byte) 0, (byte) 1) == 1){
-                        gb.undoMove(changedFields, coords, true);
-                        return 1;
-                    }
-                    gb.undoMove(changedFields, coords, true);
-                    possibleMoves ^= coords;
-                }
-                return 0;
-            }
-            if(beta == 0){
-                do{
-                    long coords = Long.highestOneBit(possibleMoves);
-                    long changedFields = gb.makeMove(true, coords);
-                    if(min(gb, (byte) -1, (byte) 0) >= 0){
-                        gb.undoMove(changedFields, coords, true);
-                        return 0;
-                    }
-                    gb.undoMove(changedFields, coords, true);
-                    possibleMoves ^= coords;
-                }while(possibleMoves != 0);
-                return -1;
-            }
-            return -1;
+            return max(red, green, alpha, beta, remainingstones - 1, Bitboard.possibleMovesRed(red, green));
         }
+        int bestvalue = beta;
+        int value = beta;
+        MoveList moveList = MoveList.recycle(moveListPool[remainingstones], possibleMoves, green, red);
+        for (int i = 0; i < moveList.size; i++){
+            int index = moveList.orderedIndices[i];
+            value = max(moveList.green[index], moveList.red[index], alpha, value, remainingstones - 1, moveList.possiblemoves[index]);
+            if(value < beta){
+                if(value <= alpha){
+                    return alpha;
+                }
+                bestvalue = value;
+            }
+        }
+        return bestvalue;
     }
 
-    public static class ExactSearch{
+    private final static int max(long red, long green, int alpha, int beta, int remainingstones, long possibleMoves){
+        nodeCount++;
+        if(timeoutNoResult){
+            return NORESULT;
+        }
+        if(remainingstones <= 4){
+            if(System.nanoTime() >= deadline){
+                timeoutNoResult = true;
+                return NORESULT;
+            }
+            return finalScorefewremainingMax(red, green, alpha, beta, false);
+        }
+        if(possibleMoves == 0){
+            if(Bitboard.possibleMovesRed(green, red) == 0){
+                return Long.bitCount(red) - Long.bitCount(green);
+            }
+            return min(red, green, alpha, beta, remainingstones - 1, Bitboard.possibleMovesRed(green, red));
+        }
+        int bestvalue = alpha;
+        int value = alpha;
+        MoveList moveList = MoveList.recycle(moveListPool[remainingstones], possibleMoves, red, green);
+        for (int i = 0; i < moveList.size; i++){
+            int index = moveList.orderedIndices[i];
+            value = min(moveList.red[index], moveList.green[index], value, beta, remainingstones - 1, moveList.possiblemoves[index]);
+            if(value > alpha){
+                if(value >= beta){
+                    return beta;
+                }
+                bestvalue = value;
+            }
+        }
+        return bestvalue;
+    }
 
+    private static int finalScorefewremainingMin(long red, long green, int alpha, int beta, boolean passed){
+        nodeCount++;
+        long emptyfields = ~(red | green);
+        long changedfields = 0;
+        long coord = 0;
+        int bestvalue = beta;
+        int value = beta;
+        boolean nomoveavailable = true;
+        while(emptyfields != 0){
+            coord = Long.highestOneBit(emptyfields);
+            changedfields = Bitboard.getflippedDiskRed(green, red, coord);
+            if(changedfields != 0){
+                value = finalScorefewremainingMax(red ^ changedfields, green
+                        ^ changedfields ^ coord, alpha, bestvalue, false);
+                if(value < beta){
+                    if(value <= alpha){
+                        return alpha;
+                    }
+                    bestvalue = value;
+                }
+                nomoveavailable = false;
+            }
+            emptyfields ^= coord;
+        }
+        if(nomoveavailable){
+            if(passed){
+                return Long.bitCount(red) - Long.bitCount(green);
+            }
+            return finalScorefewremainingMax(red, green, alpha, beta, true);
+        }
+        return bestvalue;
+    }
+
+    private static int finalScorefewremainingMax(long red, long green, int alpha, int beta, boolean passed){
+        nodeCount++;
+        long emptyfields = ~(red | green);
+        long changedfields = 0;
+        long coord = 0;
+        int bestvalue = alpha;
+        int value = alpha;
+        boolean nomoveavailable = true;
+        while(emptyfields != 0){
+            coord = Long.highestOneBit(emptyfields);
+            changedfields = Bitboard.getflippedDiskRed(red, green, coord);
+            if(changedfields != 0){
+                value = finalScorefewremainingMin(red ^ changedfields
+                        ^ coord, green ^ changedfields, bestvalue, beta, false);
+                if(value > alpha){
+                    if(value >= beta){
+                        return beta;
+                    }
+                    bestvalue = value;
+                }
+                nomoveavailable = false;
+            }
+            emptyfields ^= coord;
+        }
+        if(nomoveavailable){
+            if(passed){
+                return Long.bitCount(red) - Long.bitCount(green);
+            }
+            return finalScorefewremainingMin(red, green, alpha, beta, true);
+        }
+        return bestvalue;
+    }
+
+    private static class MoveList{
+        private static final long CORNERS = 0x8100000000000081L;
+        public long[]             possiblemoves;
+        public long[]             red;
+        public long[]             green;
+        public long[]             move;
+        public int[]              orderedScore;
+        public int[]              orderedIndices;
+        public int                size;
+
+        public static MoveList[] generateListPool(int poolSize, int listSize){
+            MoveList[] pool = new MoveList[poolSize];
+            for (int i = 0; i < pool.length; i++){
+                pool[i] = new MoveList(listSize);
+            }
+            return pool;
+        }
+
+        public MoveList(int size){
+            possiblemoves = new long[size];
+            red = new long[size];
+            green = new long[size];
+            orderedScore = new int[size];
+            orderedIndices = new int[size];
+            move = new long[size];
+        }
+
+        public final static MoveList recycle(MoveList oldlist, long possiblemoves, long red, long green){
+            int size = Long.bitCount(possiblemoves);
+            oldlist.size = size;
+            for (int i = 0; i < size; i++){
+                oldlist.orderedIndices[i] = i;
+                long coord = Long.highestOneBit(possiblemoves);
+                oldlist.move[i] = coord;
+                long flipeddisk = Bitboard.getflippedDiskRed(red, green, coord);
+                long newred = red ^ flipeddisk ^ coord;
+                long newgreen = green ^ flipeddisk;
+                oldlist.red[i] = newred;
+                oldlist.green[i] = newgreen;
+                long newpossiblemoves = Bitboard.possibleMovesRed(newgreen, newred);
+                oldlist.possiblemoves[i] = newpossiblemoves;
+                long occupiedCorners = newred & CORNERS;
+                long cornerstability = occupiedCorners;
+                cornerstability |= occupiedCorners >>> 1 & 0x7f7f7f7f7f7f7f7fL;
+                cornerstability |= occupiedCorners >>> 8;
+                cornerstability |= occupiedCorners << 1 & 0xfefefefefefefefeL;
+                cornerstability |= occupiedCorners << 8;
+                cornerstability &= newred;
+                int score = 8 * Long.bitCount(cornerstability) - 8
+                        * Long.bitCount(newpossiblemoves & CORNERS)
+                        - Long.bitCount(newpossiblemoves);
+                oldlist.orderedScore[i] = score;
+                possiblemoves ^= coord;
+            }
+            insertionSort(oldlist.orderedScore, oldlist.orderedIndices, size);
+            return oldlist;
+        }
+
+        private final static void insertionSort(int[] orderedScore, int[] orderedIndices, int size){
+            for (int i = 1; i < size; i++){
+                int j;
+                int tempScore = orderedScore[i];
+                int tempIndex = orderedIndices[i];
+                for (j = i - 1; j >= 0 && tempScore > orderedScore[j]; j--){
+                    orderedScore[j + 1] = orderedScore[j];
+                    orderedIndices[j + 1] = orderedIndices[j];
+                }
+                j++;
+                orderedScore[j] = tempScore;
+                orderedIndices[j] = tempIndex;
+            }
+        }
     }
 }
