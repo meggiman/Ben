b22ca808f33905c2e6cb53a5c06566db9bfd6c5a
Noah Huesser <noah.huesser@gmail.com>
2014-05-25 20:56:35 +0200

    latest build
    Added new weights (with some fuckup), added disclaimer, cleaned some
    shit

--- src/Ben/Ben.java
+++ src/Ben/Ben.java
@@ -1,3 +1,19 @@
+/*********************************************************************************************/
+/** Authors:                                                                                **/
+/**     Manuel Eggimann (meggimann@student.ethz.ch)                                         **/
+/**     Noah Huesser (nhuesser@student.ethz.ch)                                             **/
+/**                                                                                         **/
+/** This program was written with the help of the internet.                                 **/
+/** Most of our knowledge was aquired on:                                                   **/
+/**     http://chessprogramming.wikispaces.com                                              **/
+/**     http://redalt.com/working-in-reversi-part-3                                         **/
+/** Furthermore we analyzed the code of past winners (namely Xiaolong and glhf.GG),         **/
+/** took some useful code and modified it for our use. Namely this is in the eval           **/
+/** to figure out the stable disks, as well as some weighting of the different features     **/
+/** Every piece of code we used from other sources than our own mind is well understood     **/
+/** and used with the thought that it is the best solution!                                 **/
+/**                                                                                         **/
+/*********************************************************************************************/
 package Ben;
 
 import reversi.Coordinates;
@@ -12,8 +28,6 @@ public class Ben implements ITestablePlayer{
     /**
      * Bitmasks
      */
-    private static final long   leftshiftMask      = 0xFEFEFEFEFEFEFEFEL;
-    private static final long   rightshiftMask     = 0x7F7F7F7F7F7F7F7FL;
     private static final long   shiftMask          = 0x7E7E7E7E7E7E7E7EL;
 
     private static final long   CORNERS            = 0x8100000000000081L;
@@ -25,39 +39,36 @@ public class Ben implements ITestablePlayer{
     private static final long   HORIZONTALEDGES    = LEFTEDGE | RIGHTEDGE;
     private static final long   EDGES              = VERTICALEDGES | HORIZONTALEDGES;
 
-    static final byte           maskC              = 0b01000010;
-    static final byte           maskA              = 0b00100100;
-    static final byte           maskB              = 0b00011000;
+    private static final byte   maskC              = 0b01000010;
+    private static final byte   maskA              = 0b00100100;
+    private static final byte   maskB              = 0b00011000;
 
-    static final long           cornerTopLeft      = 0x8000000000000000L;
-    static final long           cornerTopRight     = 0x100000000000000L;
-    static final long           cornerBotLeft      = 128;
-    static final long           cornerBotRight     = 1;
-    static final long           xTopLeft           = 0x40000000000000L;
-    static final long           xTopRight          = 0x2000000000000L;
-    static final long           xBotLeft           = 0x4000;
-    static final long           xBotRight          = 0x200;
+    // Corners, C-pieces, z-pieces, edges w/o corner or c-pieces, other pieces
+    private static final long[] scoreCategories    = { 0x8100000000000081L, 0x4281000000008142L, 0x0024000000002400L, 0x3c0081818181003cL, 0x003c5a7e7e5a3c00L };
+    private static final int[]  squareScoreWeight  = { 96, -3, -5, 2, -2 };
 
     /*
+     * Format of the board
      * 012
      * 7 3
      * 654
      */
     // bitmasks for every line-direction
-    private static long         LINES15[]          = {
+    // used to calculate stable pieces
+    private static final long   LINES15[]          = {
                                                    0x0101010101010101L, 0x0202020202020202L, 0x0404040404040404L, 0x0808080808080808L,
                                                    0x1010101010101010L, 0x2020202020202020L, 0x4040404040404040L, 0x8080808080808080L
                                                    };
-    private static long         LINES37[]          = {
+    private static final long   LINES37[]          = {
                                                    0x00000000000000ffL, 0x000000000000ff00L, 0x0000000000ff0000L, 0x00000000ff000000L,
                                                    0x000000ff00000000L, 0x0000ff0000000000L, 0x00ff000000000000L, 0xff00000000000000L
                                                    };
-    private static long         LINES04[]          = {
+    private static final long   LINES04[]          = {
                                                    0x8040201008040201L, 0x0080402010080402L, 0x0000804020100804L, 0x0000008040201008L,
                                                    0x0000000080402010L, 0x0000000000804020L, 0x4020100804020100L, 0x2010080402010000L,
                                                    0x1008040201000000L, 0x0804020100000000L, 0x0402010000000000L
                                                    };
-    private static long         LINES26[]          = {
+    private static final long   LINES26[]          = {
                                                    0x0102040810204080L, 0x0001020408102040L, 0x0000010204081020L, 0x0000000102040810L,
                                                    0x0000000001020408L, 0x0000000000010204L, 0x0204081020408000L, 0x0408102040800000L,
                                                    0x0810204080000000L, 0x1020408000000000L, 0x2040800000000000L
@@ -113,9 +124,9 @@ public class Ben implements ITestablePlayer{
     private static byte[]       TTdepths           = new byte[TTsize];
     private static byte[]       TTtypes            = new byte[TTsize];
     private static byte[]       TTplayedStones     = new byte[TTsize];
-
     private static long         hash;
 
+    // Sorted moves
     private static long[][]     SboardsRed         = new long[30][30];
     private static long[][]     SboardsGreen       = new long[30][30];
     private static int[][]      Svalues            = new int[30][30];
@@ -428,16 +439,7 @@ public class Ben implements ITestablePlayer{
         return size;
     }
 
-    private final static long PMO               = 0x8100000000000081L;
-    // private final static long PMC = 0x4281000000008142L;
-    // private final static long PMX = 0x0042000000004200L;
-    private final static long PME               = 0x3C0081818181003CL;
-
-    // Corners, C pieces, weird peices, edges without corner or c, all other
-    // fields
-    private static long[]     scoreCategories   = { 0x8100000000000081L, 0x4281000000008142L, 0x0024000000002400L, 0x3c0081818181003cL, 0x003c5a7e7e5a3c00L };
-    public static int[]       squareScoreWeight = { 90, -3, -5, 2, -2 };
-
+    // Help from Xiaolong and glhf.GG
     private static final short evaluate(long red, long green, long possibleMovesRedLong, long possibleMovesGreenLong){
         long empty = ~(red | green);
 
@@ -447,107 +449,12 @@ public class Ben implements ITestablePlayer{
             squareScore += squareScoreWeight[i] * (Long.bitCount(red & scoreCategories[i]) - Long.bitCount(green & scoreCategories[i]));
         }
 
-        return (short) ((((red | green) & PMO) == 0
-                ? (Long.bitCount(red) < 2 ? -30 : 0)
+        return (short) ((((red | green) & scoreCategories[0]) == 0 ? (Long.bitCount(red) < 2 ? -30 : 0)
                 : 12 * (Long.bitCount(getStableDisks(red, green)) - Long.bitCount(getStableDisks(green, red))))
                 + 10 * (Long.bitCount(fillAdjacent(empty) & green) - Long.bitCount(fillAdjacent(empty) & red))
                 + 5 * (Long.bitCount(possibleMovesRedLong & 0xbd3cffffffff3cbdL) - Long.bitCount(possibleMovesGreenLong & 0xbd3cffffffff3cbdL))
-                + 1 * (Long.bitCount(red & PME) - Long.bitCount(green & PME))
-                + 24 * (Long.bitCount(red & PMO) - Long.bitCount(green & PMO))
                 + (Long.bitCount(empty) % 2 == 1 ? 50 : -50)
                 + squareScore);
-    }
-
-    private static final short evaluate(long red, long green, long possibleMovesRedLong, long possibleMovesGreenLong, boolean bloeb){
-        int stonesRed = countStones(red);
-        int stonesGreen = countStones(green);
-
-        int discCount = stonesRed + stonesGreen;
-        double EC = 4 * (discCount < 50 ? 1 : 4);
-        double MC = 8 * (discCount < 30 ? 3 : 2);
-        double MC2 = 10 * (discCount < 30 ? 6 : 0.5);
-        double SC = 1.8 * (discCount < 30 ? 1 : 4);
-
-        // Mobility
-        int possibleMovesRed = Long.bitCount(possibleMovesRedLong & 0xbd3cffffffff3cbdL);
-        int possibleMovesGreen = Long.bitCount(possibleMovesGreenLong & 0xbd3cffffffff3cbdL);
-
-        // Potential mobility
-        long empty = ~(red | green);
-        int potentialMovesRed = Long.bitCount(Bitboard.fillAdjacent(empty)
-                & green);
-        int potentialMovesEnemyGreen = Long.bitCount(Bitboard.fillAdjacent(empty)
-                & red);
-
-        int parity = 0;
-        if(discCount % 2 == 1){
-            parity += 50;
-        }
-        else{
-            parity -= 50;
-        }
-
-        // Edge advantage
-        int edgeAdvantage = (int) getEdgeValue(red, green);
-        if((xTopLeft & red) != 0 && (cornerTopLeft & red) == 0){
-            edgeAdvantage -= 40;
-        }
-        if((xTopRight & red) != 0 && (cornerTopRight & red) == 0){
-            edgeAdvantage -= 40;
-        }
-        if((xBotRight & red) != 0 && (cornerBotRight & red) == 0){
-            edgeAdvantage -= 40;
-        }
-        if((xBotLeft & red) != 0 && (cornerBotLeft & red) == 0){
-            edgeAdvantage -= 40;
-        }
-
-        if((xTopLeft & green) != 0 && (cornerTopLeft & green) == 0){
-            edgeAdvantage -= 40;
-        }
-        if((xTopRight & green) != 0 && (cornerTopRight & green) == 0){
-            edgeAdvantage -= 40;
-        }
-        if((xBotRight & green) != 0 && (cornerBotRight & green) == 0){
-            edgeAdvantage -= 40;
-        }
-        if((xBotLeft & green) != 0 && (cornerBotLeft & green) == 0){
-            edgeAdvantage -= 40;
-        }
-
-        // Mobility advantage
-        float mobilityAdvantage = possibleMovesRed - possibleMovesGreen;
-
-        // Potential mobility advantage
-        float potentialMobilityAdvantage = potentialMovesRed -
-                potentialMovesEnemyGreen;
-
-        int occupiedSquareAdvantage = (int) (Long.bitCount(getStableDisks(red,
-                green))
-                - Long.bitCount(getStableDisks(green, red)));
-
-        int score = (int) (
-                EC * edgeAdvantage
-                        + MC * mobilityAdvantage
-                        + MC2 * potentialMobilityAdvantage
-                        + SC * occupiedSquareAdvantage
-                );
-        if(score > 32767 || score < -32768){
-            System.out.println("ALERT!SWEG!11ELF!!");
-        }
-        // gb.print();
-        // System.out.println("Evaluator Noah");
-        // System.out.println("EdgeAdvantage: " + EC * edgeAdvantage);
-        // System.out.println("MobilityAdvantage: " + (MC * mobilityAdvantage));
-        // System.out.println("PotentialMobility: "
-        // + (MC2 * potentialMobilityAdvantage));
-        // System.out.println("occupiedSquareAdvantage: "
-        // + SC * occupiedSquareAdvantage);
-        // System.out.println(score);
-        // System.out.println("------------------------");
-        // System.out.println("Evaluator Xiaolon");
-        // System.out.println(testXiaolong.evaluate(gb.red, gb.green));
-        return (short) score;
     }
 
     /**
@@ -1245,40 +1152,9 @@ public class Ben implements ITestablePlayer{
         return stable;
     }
 
-    private static final long getStableDisks(long red, long green){
+    // Taken from Xiaolong
+    private static final long getStableDisks(final long red, final long green){
         long current = 0, before = red | green, filled04 = EDGES, filled15 = VERTICALEDGES, filled26 = EDGES, filled37 = HORIZONTALEDGES;
-        int i;
-        for (i = 0; i < 8; i++){
-            if((before & LINES15[i]) == LINES15[i]){
-                filled15 |= LINES15[i];
-            }
-            if((before & LINES37[i]) == LINES37[i]){
-                filled37 |= LINES37[i];
-            }
-        }
-        for (i = 0; i < 11; i++){
-            if((before & LINES04[i]) == LINES04[i]){
-                filled04 |= LINES04[i];
-            }
-            if((before & LINES26[i]) == LINES26[i]){
-                filled26 |= LINES26[i];
-            }
-        }
-
-        while(current != before){
-            before = current;
-            current |= red
-                    & ((current << 8) | (current >>> 8) | filled15) // 15
-                    & ((current << 1) | (current >>> 1) | filled37) // 37
-                    & ((current << 9) | (current >>> 9) | filled04) // 04
-                    & ((current << 7) | (current >>> 7) | filled26); // 26
-        }
-        return current;
-    }
-
-    private static final long getStableDisks(final long red, final long green, final boolean player){
-        long current = 0, before = red | green, filled04 = EDGES, filled15 = VERTICALEDGES, filled26 = EDGES, filled37 = HORIZONTALEDGES;
-
         int i;
         for (i = 0; i < 8; i++){
             if((before & LINES15[i]) == LINES15[i]){
@@ -1932,7 +1808,6 @@ public class Ben implements ITestablePlayer{
         Ben.myColor = myColor;
         Ben.timeLimit = timeLimit * 1000000;
         playedStones = (myColor == GameBoard.RED) ? (byte) 2 : (byte) 3;
-        generateEdgeTable();
         validsearch.evaluator = new StrategicEvaluatorNoah();
     }
 
